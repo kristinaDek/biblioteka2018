ad5dd413ce8d3f4f7a282f005658b73970c7d30e
kristinaDek <kristina.dekic8@gmail.com>
2018-03-02 14:59:12 +0100

    Dodati atributi u klasu Knjiga

--- Biblioteka/src/biblioteka/Knjiga.java
+++ Biblioteka/src/biblioteka/Knjiga.java
@@ -1,5 +1,43 @@
+
 package biblioteka;
 
 public class Knjiga {
-
+	private String naslov;
+	private String isbn;
+	private String autiori;
+	private String izdavac;
+	private int izdanje;
+	
+	public String getNaslov() {
+		return naslov;
+	}
+	public void setNaslov(String naslov) {
+		this.naslov = naslov;
+	}
+	public String getIsbn() {
+		return isbn;
+	}
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+	public String getAutiori() {
+		return autiori;
+	}
+	public void setAutiori(String autiori) {
+		this.autiori = autiori;
+	}
+	public String getIzdavac() {
+		return izdavac;
+	}
+	public void setIzdavac(String izdavac) {
+		this.izdavac = izdavac;
+	}
+	public int getIzdanje() {
+		return izdanje;
+	}
+	public void setIzdanje(int izdanje) {
+		this.izdanje = izdanje;
+	}
+	
+	
 }
